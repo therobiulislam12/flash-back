125115cae055723e37166e15ad011336d67f7ba8
Robiul Islam <robiul215565@gmail.com>
2022-11-28 10:43:48 +0600

    Advertisement done

--- src/components/Advertisement.js
+++ src/components/Advertisement.js
@@ -1,45 +1,25 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Category from "./Category";
 
 const Advertisement = () => {
   const [advertisementItems, setAdvertisementItems] = useState({});
 
+  useEffect(() =>{
+    axios('http://localhost:5000/advertisementItems').then(advertisement => setAdvertisementItems(advertisement.data))
+  }, [])
+  
+
   return (
-    advertisementItems && (
+    advertisementItems.length > 0 && (
       <div className="py-8 px-4 lg:py-12 bg-[#00E3AA] rounded-lg shadow my-8 lg:my-12">
         <h2 className="text-2xl lg:text-3xl font-bold text-white text-center uppercase mb-6 lg:mb-12">
           Advisement Products
         </h2>
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <div className="shadow-lg rounded-md p-4 text-center cursor-pointer hover:-translate-y-2 transition bg-gray-50 space-y-4 lg:space-y-6 border">
-            <img
-              src="https://www.cameralabs.com/wp-content/uploads/2019/08/canon-eos-90d-hero-1.jpg"
-              alt="name"
-              className="w-full rounded h-56"
-            />
-            <div className={"text-center"}>
-              <h2 className="text-2xl font-semibold">DSLR Cameras</h2>
-            </div>
-          </div>
-          <div className="shadow-lg rounded-md p-4 text-center cursor-pointer hover:-translate-y-2 transition bg-gray-50 space-y-4 lg:space-y-6 border">
-            <img
-              src="https://cdn.pocket-lint.com/r/s/1201x/assets/images/137301-cameras-news-buyer-s-guide-best-360-camera-lead-image15-vh3apnaeiz.jpg"
-              alt="name"
-              className="w-full rounded h-56"
-            />
-            <div className={"text-center"}>
-              <h2 className="text-2xl font-semibold">DSLR Cameras</h2>
-            </div>
-          </div>
-          <div className="shadow-lg rounded-md p-4 text-center cursor-pointer hover:-translate-y-2 transition bg-gray-50 space-y-4 lg:space-y-6 border">
-            <img
-              src="https://cdn.pocket-lint.com/r/s/1201x/assets/images/137301-cameras-news-buyer-s-guide-best-360-camera-lead-image15-vh3apnaeiz.jpg"
-              alt="name"
-              className="w-full rounded h-56"
-            />
-            <div className={"text-center"}>
-              <h2 className="text-2xl font-semibold">DSLR Cameras</h2>
-            </div>
-          </div>
+        {
+          advertisementItems.map(advertisementItem => <Category key={advertisementItem._id} category={advertisementItem} showSelectedCategory/>)
+        }
         </div>
       </div>
     )
